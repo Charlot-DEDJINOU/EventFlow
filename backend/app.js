7cb552facf4a49d7d01a4e525543814a2728c6c2
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-09 01:01:53 +0100

    switch condition

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const databaseInit = require("./database/databaseInit");
-const databaseConfig = require('./database/database');
+const InviteController = require("./controller/InviteController");
 const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 3001 });
@@ -8,46 +8,57 @@ databaseInit.initializeDatabase();
 
 let clients = new Set();
 
-let db = databaseConfig.createDb() ;
-
-server.on('connection', (ws) => {
+server.on('connection', async (ws) => {
 
   clients.add(ws);
   console.log('Nouvelle connexion WebSocket établie.');
 
-  db.all('SELECT * FROM message', (err, rows) => {
-    if (err) {
-      console.error('Erreur lors de la récupération des messages :', err);
-    } else {
-      
-      rows.forEach((row) => {
-        ws.send(JSON.stringify(row));
-      });
+  ws.send(JSON.stringify(await InviteController.getInvites())) ;
+
+  ws.on('message', async (message) => {
+    const data = JSON.parse(message);
+    switch (data.action) {
+      case 'getinvites':
+        ws.send(JSON.stringify(await InviteController.getInvites())) ;
+        break;
+      case 'addinvite':
+        console.log('Invite reçu :', data.item);
+        InviteController.addInvite(data.item);
+
+        const newinvite = {
+          action: 'add',
+          item: data.item
+        };
+
+        clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(newinvite));
+          }
+        });
+        break;
+      case 'updateinvite':
+        console.log('Invite reçu :', data.item);
+        InviteController.update(data.item, data.item.id);
+
+        const updateinvite = {
+          action: 'update',
+          item: data.item
+        };
+
+        clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(updateinvite));
+          }
+        });
+        break;
     }
-  });
-
-  ws.on('message', (message) => {
-    console.log('Message reçu :', message);
-
-    message = JSON.parse(message) ;
-    
-    db.run('INSERT INTO message (destinateur, recepteur ,message) VALUES (?,?,?)', [message.destinateur , message.recepteur , message.message], (err) => {
-      if (err) {
-        console.error('Erreur lors de l\'enregistrement du message :', err);
-      }
-    });
-
-    clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(message));
-      }
-    });
   });
 
   ws.on('close', () => {
     clients.delete(ws);
     console.log('Connexion WebSocket fermée.');
   });
+
 });
 
 console.log('Serveur WebSocket en cours d\'exécution sur le port 3001');
