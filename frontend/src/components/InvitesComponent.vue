1973c64e8998d5f43b2c1f5be50f6999ccf396b9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-11 15:11:33 +0100

    InvitesComponent

---
+++ frontend/src/components/InvitesComponent.vue
@@ -0,0 +1,14 @@
+<script>
+import InviteItem from './InviteItem';
+
+export default {
+    components : {
+        InviteItem
+    }
+}
+</script>
+<template>
+    <div class="w-100 mt-5">
+        <InviteItem />
+    </div>
+</template>
