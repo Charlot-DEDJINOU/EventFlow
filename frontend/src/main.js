40b51a8986fcc42ed71106755340d89f3ad36f29
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-11 15:14:22 +0100

    add bootstrap path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+// eslint-disable-next-line no-unused-vars
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
