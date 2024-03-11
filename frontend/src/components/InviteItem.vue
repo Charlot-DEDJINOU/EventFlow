33fc66d336e10a2927b9bf8af86c929345cfc6f6
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-11 15:11:48 +0100

    InviteItem

---
+++ frontend/src/components/InviteItem.vue
@@ -0,0 +1,18 @@
+<script>
+import EditIcon from './icons/EditIcon';
+
+export default {
+    component : {
+        EditIcon
+    }
+}
+</script>
+<template>
+    <div class="item w-100 p-2 justify-content-between align-items-center">
+        <div class="w-75">
+            <p>Charlot DEDJINOU</p>
+            <span>59 10 52 67</span>
+        </div>
+        <EditIcon />
+    </div>
+</template>
