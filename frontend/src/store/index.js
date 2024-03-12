73dacd6557433090099aa74977c8c9540cf40f6f
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-12 08:59:36 +0100

    index store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    invites: []
+  },
+  getters: {},
+  mutations: {
+    resetInvites(state, invites) {
+        state.invites = invites
+    },
+    addInvite(state, invite) {
+      state.invites.push(invite)
+    },
+    updateInvite(state, updatedInvite) {
+      for (let i = 0; i < state.invites.length; i++) {
+        if (state.invites[i].id === updatedInvite.id) {
+          state.invites[i] = updatedInvite
+          break
+        }
+      }
+    }
+  },
+  actions: {
+    ToggleInvites({ commit }, invites) {
+      commit('addInvite', invites)
+    },
+    ToggleInvite({ commit }, invite) {
+      commit('updateInvite', invite)
+    },
+    ResetInvites({ commit }, invites) {
+      commit('resetInvites', invites)
+    }
+  },
+  modules: {}
+})
