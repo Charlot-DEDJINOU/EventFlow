7a5ac296fa28d495e76560b3ab2c8933a7374ca2
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-12 09:00:30 +0100

    move socket.send to BodyComponent

--- frontend/src/websocket/index.js
+++ frontend/src/websocket/index.js
@@ -1,22 +1,31 @@
-// websocket.js
-
-const socket = new WebSocket('ws://localhost:8080');
+export const socket = new WebSocket('ws://localhost:8080')
 
 socket.onopen = function () {
-  console.log('Connexion WebSocket établie. ');
-};
-
-socket.onmessage = function (event) {
-  const message = event.data;
-  console.log('Message reçu du serveur : ', message);
-};
+  console.log('Connexion WebSocket établie. ')
+}
 
 socket.onerror = function () {
-  console.error('Erreur WebSocket : ');
-};
+  console.error('Erreur WebSocket : ')
+}
 
 socket.onclose = function () {
-  console.log('Connexion WebSocket fermée ');
-};
+  console.log('Connexion WebSocket fermée')
+}
 
-export default socket;
+export const updateState = (message, store) => {
+  switch (message.action) {
+    case 'invites':
+      store.dispatch('ResetInvites', message.items);
+      break
+    case 'add' : 
+      store.dispatch('ToggleInvites', message.item); 
+      break 
+    case 'update':
+      store.dispatch('ToggleInvite', message.item)
+      break 
+  }
+}
+
+export const sendMessage = (data) => {
+    socket.send(JSON.stringify(data));
+}
