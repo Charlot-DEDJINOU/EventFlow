deeec160b67c22cd95e332d54e0e09b57e24dcba
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-15 17:43:05 +0100

    dynamic Succussful Component

---
+++ frontend/src/components/SuccessfulComponent.vue
@@ -0,0 +1,17 @@
+<script>
+import CheckIcon from './icons/CheckIcon.vue'
+export default {
+  props: {
+    title: String
+  },
+  components: {
+    CheckIcon
+  }
+}
+</script>
+<template>
+  <div class="successful d-flex flex-column justify-content-center align-items-center">
+    <CheckIcon class="text-success" />
+    <p class="my-3 fs-4 fw-medium">{{ title }}</p>
+  </div>
+</template>
