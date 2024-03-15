df53a7bf1d65fcb428bb2a89db4434ab98bd1514
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-15 17:40:54 +0100

    Component Invite

--- frontend/src/components/InviteItem.vue
+++ frontend/src/components/InviteItem.vue
@@ -2,17 +2,27 @@
 import EditIcon from './icons/EditIcon.vue'
 
 export default {
-  props : {
-    invite : Object
+  props: {
+    invite: Object
   },
   components: {
     EditIcon
+  },
+  setup() {
+    const ObjectToString = (data) => {
+      return JSON.stringify(data)
+    }
+
+    return {
+      ObjectToString
+    }
   }
 }
 </script>
 <template>
   <div
-    class="item w-100 p-2 d-flex justify-content-between align-items-center invite mb-2 px-3" :class="invite.is_entry === 0 ? 'text-bg-danger' : 'text-bg-success'"
+    class="item w-100 p-2 d-flex justify-content-between align-items-center invite mb-2 px-3"
+    :class="invite.is_entry === 0 ? 'text-bg-danger' : 'text-bg-success'"
   >
     <div class="d-flex flex-wrap justify-content-between align-items-center" style="width: 80%">
       <div class="m-2 d-flex flex-column w-50">
@@ -20,14 +30,32 @@ export default {
         <span class="m-2">{{ invite.status }}</span>
         <span class="m-2">{{ invite.numero }}</span>
       </div>
-      
-      <span class="m-2 fw-bold">{{ invite.is_entry === 0 ? 'Present' : 'Absent'}}</span>
+
+      <span class="m-2 fw-bold">{{ invite.is_entry == 1 ? 'Present' : 'Absent' }}</span>
     </div>
-    <EditIcon class="text-white fw-bold d-inline-block m-2" />
+    <router-link
+      :to="
+        '/update/' +
+        invite.id +
+        '/' +
+        invite.appelation +
+        '/' +
+        invite.numero +
+        '/' +
+        invite.status +
+        '/' +
+        invite.is_entry
+      "
+    >
+      <EditIcon class="text-white fw-bold d-inline-block m-2 cursor" />
+    </router-link>
   </div>
 </template>
 <style scoped>
 .invite {
   border-radius: 5px;
+}
+.cursor {
+  cursor: pointer;
 }
 </style>
