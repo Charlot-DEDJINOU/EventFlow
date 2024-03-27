e5bf089ff1d7c073f5ddbd2a755f141518ce9955
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-28 00:25:07 +0100

    mongo connection

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,57 +1,22 @@
-const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    user: 'charlot',
-    host: 'dpg-co214go21fec73d4kphg-a',
-    database: 'gestion_p99u',
-    password: 'ZOsz669dzuCFo5GgjGj6qi66C0KTyoqc',
-    port: 5432, 
-});
+const { MongoClient } = require('mongodb');
 
-let client;
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri);
+let db;
 
-async function connectToDatabase() {
-    try {
-        if (!client || client._ending) {
-            client = await pool.connect();
-            console.log('Connecté à PostgreSQL');
-        }
-        return client;
-    } catch (error) {
-        console.error('Erreur de connexion à PostgreSQL :', error);
-        throw error;
+async function connectDB() {
+  try {
+    if(!db) {
+        const conn = await client.connect();
+        db = conn.db(process.env.DATABASE)
+        console.log('Connected to MongoDB');
     }
+    return db
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
 
-function closeConnection() {
-    if (client) {
-        client.release();
-        console.log('Connexion à PostgreSQL fermée');
-    }
-}
-
-async function createInvitesTable() {
-    const client = await connectToDatabase();
-    try {
-        await client.query(`
-            CREATE TABLE IF NOT EXISTS invites (
-                id SERIAL PRIMARY KEY,
-                appelation VARCHAR(255),
-                numero VARCHAR(50),
-                status VARCHAR(50),
-                sexe VARCHAR(10),
-                email VARCHAR(255),
-                is_boursier BOOLEAN,
-                is_entry BOOLEAN
-            )
-        `);
-        console.log('Table "invites" créée avec succès ou déjà existante');
-    } catch (error) {
-        console.error('Erreur lors de la création de la table "invites" :', error);
-        throw error;
-    } finally {
-        client.release(); // Libérer le client de la pool de connexions
-    }
-}
-
-module.exports = { connectToDatabase, createInvitesTable, closeConnection };
+module.exports = { connectDB };
