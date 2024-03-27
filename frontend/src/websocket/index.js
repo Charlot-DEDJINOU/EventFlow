36f75ee8c515d74cb10b346ecdb1bad45d03104a
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-27 09:09:29 +0100

    change route for my websocket

--- frontend/src/websocket/index.js
+++ frontend/src/websocket/index.js
@@ -1,4 +1,4 @@
-export const socket = new WebSocket('wss://eventflow-iazd.onrender.com')
+export const socket = new WebSocket('ws://localhost:8080')
 
 socket.onopen = function () {
   console.log('Connexion WebSocket établie. ')
