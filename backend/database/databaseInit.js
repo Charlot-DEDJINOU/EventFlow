f8dab7c516affa00dbe816724fb18b19364df319
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-27 09:06:15 +0100

    update

--- backend/database/databaseInit.js
+++ backend/database/databaseInit.js
@@ -6,8 +6,6 @@ function initializeDatabase() {
     if (!exist) {
       console.log("La base de données n'existe pas. Création en cours...");
       databaseConfig.createTableInvites(db);
-      databaseConfig.insertInvites(db);
-      console.log("Donnee insérer avec succes.");
     } else {
       console.log("Les tables existent déjà.");
     }
