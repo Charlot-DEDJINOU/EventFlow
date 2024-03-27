e019eb048feec8b66f0af407a9798ebd473cc496
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-28 00:24:51 +0100

    mongo logic

--- backend/controller/InviteController.js
+++ backend/controller/InviteController.js
@@ -1,84 +1,52 @@
-const { connectToDatabase, closeConnection } = require("../database/database");
+const { connectDB } = require("../database/database");
+const { ObjectId } = require('mongodb');
 
 const addInvite = async (item) => {
-    let client;
     try {
-        const client = await connectToDatabase();
-        const { appelation, numero, status, sexe, email, is_boursier, is_entry } = item;
-        const queryText = `
-            INSERT INTO invites (appelation, numero, status, sexe, email, is_boursier, is_entry)
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-            RETURNING id
-        `;
-        const values = [appelation, numero, status, sexe, email, is_boursier, is_entry];
-        const result = await client.query(queryText, values);
-        console.log('Document inséré avec succès:', result.rows[0].id);
-        return result.rows[0].id;
+        const db = await connectDB()
+        const collection = db.collection(process.env.COLLECTION);
+        const result = await collection.insertOne(item);
+        console.log('Document inséré avec succès:', result.insertedId);
+        return result.insertedId;
     } catch (error) {
         console.error("Erreur lors de l'insertion :", error);
         throw error;
-    }  finally {
-        if (client) {
-            closeConnection();
-        }
     }
 };
 
 const getInvites = async () => {
-    let client;
     try {
-        const client = await connectToDatabase();
-        const queryText = `SELECT * FROM invites`;
-        const result = await client.query(queryText);
-        return result.rows;
+        const db = await connectDB()
+        const collection = db.collection(process.env.COLLECTION);
+        const result = await collection.find({}).toArray();
+        return result;
     } catch (error) {
         console.error('Erreur lors de la récupération des invites :', error);
         throw error;
-    }  finally {
-        if (client) {
-            closeConnection();
-        }
     }
 };
 
 const updateInvite = async (item, id) => {
-    let client;
     try {
-        const client = await connectToDatabase();
-        const { appelation, numero, status, sexe, email, is_boursier, is_entry } = item;
-        const queryText = `
-            UPDATE invites 
-            SET appelation = $1, numero = $2, status = $3, sexe = $4, email = $5, is_boursier = $6, is_entry = $7
-            WHERE id = $8
-        `;
-        const values = [appelation, numero, status, sexe, email, is_boursier, is_entry, id];
-        const result = await client.query(queryText, values);
+        const db = await connectDB()
+        const collection = db.collection(process.env.COLLECTION);
+        const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: item });
         console.log('Document mis à jour avec succès');
     } catch (error) {
         console.error("Erreur lors de la mise à jour :", error);
         throw error;
-    }  finally {
-        if (client) {
-            closeConnection();
-        }
     }
 };
 
 const deleteInvite = async (id) => {
-    let client;
     try {
-        const client = await connectToDatabase();
-        const queryText = `DELETE FROM invites WHERE id = $1`;
-        const values = [id];
-        const result = await client.query(queryText, values);
+        const db = await connectDB()
+        const collection = db.collection(process.env.COLLECTION);
+        const result = await collection.deleteOne({ _id: id });
         console.log('Document supprimé avec succès');
     } catch (error) {
         console.error("Erreur lors de la suppression :", error);
         throw error;
-    }  finally {
-        if (client) {
-            closeConnection();
-        }
     }
 };
 
