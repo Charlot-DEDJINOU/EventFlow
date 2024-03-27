85c3fe608c1b5863746412b09cd76daa00dc87a0
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-28 00:28:07 +0100

    add champ nom

--- frontend/src/components/InviteItem.vue
+++ frontend/src/components/InviteItem.vue
@@ -22,27 +22,29 @@ export default {
 <template>
   <div
     class="item w-100 p-2 d-flex justify-content-between align-items-center invite mb-2 px-3"
-    :class="invite.is_entry == 0 ? 'text-bg-danger' : 'text-bg-success'"
+    :class="invite.present === 'Non' ? 'text-bg-danger' : 'text-bg-success'"
   >
     <div class="d-flex flex-wrap justify-content-between align-items-center" style="width: 80%">
       <div class="m-2 d-flex flex-column w-50">
-        <span class="m-2">{{ invite.appelation }}</span>
+        <span class="m-2">{{ invite.nom + ' ' + invite.prenom }}</span>
         <span class="m-2">{{ invite.sexe }}</span>
         <span class="m-2">{{ invite.status }}</span>
-        <span class="m-2">{{ invite.numero }}</span>
+        <span class="m-2">{{ invite.telephone }}</span>
         <span class="m-2">{{ invite.email }}</span>
       </div>
 
-      <span class="m-2 fw-bold">{{ invite.is_entry == 1 ? 'Present' : 'Absent' }}</span>
+      <span class="m-2 fw-bold">{{ invite.present === "Oui" ? 'Present' : 'Absent' }}</span>
     </div>
     <router-link
       :to="
         '/update/' +
-        invite.id +
+        invite._id +
         '/' +
-        invite.appelation +
+        invite.nom +
         '/' +
-        invite.numero +
+        invite.prenom +
+        '/' +
+        invite.telephone +
         '/' +
         invite.status +
         '/' +
@@ -50,9 +52,9 @@ export default {
         '/' +
         invite.sexe +
         '/' +
-        invite.is_boursier +
+        invite.boursier +
         '/' +
-        invite.is_entry
+        invite.present
       "
     >
       <EditIcon class="text-white fw-bold d-inline-block m-2 cursor" />
