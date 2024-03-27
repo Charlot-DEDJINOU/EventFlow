6a4ad24dfdf62384b5433817ff7acd4ae2adf0a9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-28 00:29:40 +0100

    socket uti

--- frontend/src/websocket/index.js
+++ frontend/src/websocket/index.js
@@ -1,4 +1,4 @@
-export const socket = new WebSocket('wss://eventflow-6aoy.onrender.com')
+export const socket = new WebSocket('ws://localhost:8080')
 
 socket.onopen = function () {
   console.log('Connexion WebSocket établie. ')
