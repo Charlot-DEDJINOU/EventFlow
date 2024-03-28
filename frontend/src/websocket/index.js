0fde778084d00cbb1df430ba4293d8bd8d950b63
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-28 02:45:15 +0100

    moogo db

--- frontend/src/websocket/index.js
+++ frontend/src/websocket/index.js
@@ -1,4 +1,4 @@
-export const socket = new WebSocket('ws://localhost:8080')
+export const socket = new WebSocket('wss://eventflow-6aoy.onrender.com')
 
 socket.onopen = function () {
   console.log('Connexion WebSocket établie. ')
