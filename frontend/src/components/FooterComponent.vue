fb52db3c81214c7fde8652c1c8f8bde252fee3c1
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-03 18:27:32 +0100

    Footer Component

---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <footer class="w-full bg-black flex items-center justify-center p-3 text-white">
+    <div class="container flex flex-col items-center">
+      <b class="text-center mb-3 text-3xl text-success">Event Flow</b>
+      <p class="text-center">
+        L’être humain à le potentiel de devenir quelque chose d’autre - David Goggins
+      </p>
+      <div class="copyright my-3">
+        &copy; Copyright <strong><span>HBD </span></strong>All Rights Reserved
+      </div>
+      <div>
+        Created by
+        <a
+          href="https://charlot-dedjinou.vercel.app"
+          class="font-bold text-white text-decoration-none"
+        >
+          Charlot DEDJINOU</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+<style>
+.w-full {
+  width: 100%;
+}
+.bg-black {
+  background-color: #000000;
+}
+.flex {
+  display: flex;
+}
+.items-center {
+  align-items: center;
+}
+.justify-center {
+  justify-content: center;
+}
+.container {
+  margin-right: auto;
+  margin-left: auto;
+}
+.flex-col {
+  flex-direction: column;
+}
+.mb-3 {
+  margin-bottom: 0.75rem;
+}
+.text-center {
+  text-align: center;
+}
+.text-3xl {
+  font-size: 1.875rem;
+}
+.font-bold {
+  font-weight: 700;
+}
+</style>
